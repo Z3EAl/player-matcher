31e18386833324ec51e4d22f035182371777326b
Ashray Inala <ainala@Ashrays-MacBook-Pro.local>
2024-12-13 20:31:57 -0800

    initial commit

---
+++ src/main/java/com/z3eai/playermatcher/PlayerMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.z3eai.playermatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerMatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/z3eai/playermatcher/PlayerMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z3eai.playermatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
